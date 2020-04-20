eb3753eba7a34e19ecea8f9d953cc21a34cc05c6
Kristóf <krstf456@gmail.com>
2020-04-20 13:24:40 +0200

    works

--- public/logic.js
+++ public/logic.js
@@ -1,1 +1,23 @@
 let socket = io()
+let name  = ""
+
+document.onload = () => {
+    name = prompt("vad heter du?")
+}
+
+socket.on('message', (incoming) => {
+    console.log(name)
+    let list = document.getElementById('messages')
+    let listItem = document.createElement('li')
+    listItem.innerText = incoming.name + ':' + incoming.message
+    list.appendChild(listItem)
+    //console.log(msg, '2')
+})
+
+function sendMessage() {
+    let input = document.getElementById("m")
+    let message = input.value
+    input.value = ""
+    socket.emit('message', { name, message })
+    
+}
--- server.js
+++ server.js
@@ -7,9 +7,26 @@ const port = process.env.PORT || 3005
 
 app.use(express.static(__dirname + '/public'))
 
-io.on('connection', () => {
-    console.log("A new connection")
+
+
+io.on('connection', (socket) => {
+
+    io.on('join room', (data) => {
+        socket.join(1)
+    })
+
+    console.log("A new connection")// socket motsvavar en klient
+
+    socket.on('message', (incoming) => {
+        console.log(incoming)
+        io.emit('message', incoming)//skicka till alla som e kopplade
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
 })
+
 
 
 
